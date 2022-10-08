35eca202c28586d739d10036c11b21d54f7a3205
Your Name <ransakaravi@icloud.com>
2022-10-08 23:34:41 +0530

    chanegs on forecatser

--- model/forecaster.py
+++ model/forecaster.py
@@ -42,7 +42,7 @@ class Forecaster():
         """
         assert isinstance(limit,(NoneType,int))
         assert isinstance(include_history,bool)
-        assert isinstance(add_country_holidays,bool)
+        assert isinstance(add_country_holidays,(bool,str))
         assert isinstance(weekly_seasonality,(str,DataFrame,bool))
         assert isinstance(yearly_seasonality,(str,DataFrame,bool))
 
@@ -60,24 +60,22 @@ class Forecaster():
                 self.results[dataset_name][category]['model'] = model
                 self.results[dataset_name][category]['error'] = error
                 self.results[dataset_name][category]['test'] = test
-
-    @classmethod           
-    def get_ts(cls,dataset_name,category):
-        _dataset = getattr(cls.dataset,dataset_name)
+         
+    def get_ts(self,dataset_name,category):
+        _dataset = getattr(self.dataset,dataset_name)
         ts = _dataset[[category]]
         ts.reset_index(inplace=True)
         ts.columns = ['ds','y']
-        train_idx = int(len(ts) * (1-cls.test_size))
+        train_idx = int(len(ts) * (1-self.test_size))
         train = ts[:train_idx]
         test = ts[train_idx:]
         return train,test
-    
-    @classmethod
-    def run_prophet(cls,dataset_name,category,include_history=False):
-        train,test = cls.get_ts(dataset_name,category)
+
+    def run_prophet(self,dataset_name,category,include_history=False):
+        train,test = self.get_ts(dataset_name,category)
         periods = len(test)
-        model = Prophet(holidays=cls.holidays,yearly_seasonality=cls.yearly_seasonality,weekly_seasonality=cls.weekly_seasonality)
-        if cls.add_country_holidays:model.add_country_holidays(country_name=cls.add_country_holidays)
+        model = Prophet(holidays=self.holidays,yearly_seasonality=self.yearly_seasonality,weekly_seasonality=self.weekly_seasonality)
+        if self.add_country_holidays:model.add_country_holidays(country_name=self.add_country_holidays)
         model.fit(train)
         forecast = model.make_future_dataframe(periods=periods, include_history=include_history)
         forecast = model.predict(forecast)
